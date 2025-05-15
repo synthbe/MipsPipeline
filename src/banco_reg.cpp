050a127079cb159c9f6ffd143dd5ac0c124140c0
Camila de Sousa <cmiladesousa@gmail.com>
2025-05-15 08:41:44 -0300

    alteração banco_reg

--- src/banco_reg.cpp
+++ src/banco_reg.cpp
@@ -1,21 +1,21 @@
 #include "../include/banco_reg.hpp"
 
 void banco_reg::write() {
-  while (true) {
-    wait();
-    if (we.read()) {
-      reg = wd.read();
-    }
+  if (we.read()) {
+    regs[rd.read()] = wd.read();
   }
 }
 
 void banco_reg::read() {
-  rd.write(reg);
+  rd1.write(regs[rs1.read()]);
+  rd2.write(regs[rs2.read()]);
 }
 
 banco_reg::banco_reg(sc_module_name name) : sc_module(name) {
-  SC_METHOD(read);
-  sensitive << wd << we;
+  SC_METHOD(write);
+  sensitive << clk.pos();
 
-  SC_CTHREAD(write, clk.pos());
+  SC_METHOD(read);
+  sensitive << rs1 << rs2;
 }
+
